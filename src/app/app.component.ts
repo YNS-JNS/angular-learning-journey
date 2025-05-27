7ce111a00443025fdd13f57359d58b228738d5f5
yns-jns <younessaitmbarek97@gmail.com>
2025-05-27 16:11:47 +0100

    chore: Setup AppComponent for data binding and directives demo (standalone)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
+import { CommonModule } from '@angular/common'; // Souvent importé ici pour *ngIf, *ngFor
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
+// Décorateur (@Component):
+// Ce décorateur marque une classe comme étant un composant Angular et fournit des métadonnées de configuration
 @Component({
-  selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent],
+  selector: 'app-root', // Nom de la balise HTML pour utiliser ce composant
+  standalone: true,
+  imports: [
+    CommonModule, // Nécessaire pour *ngIf, *ngFor, etc.
+    FormsModule,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
